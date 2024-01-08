92b2837ac6ff7f70eb97c710cb556ef1453235c8
rnutall <rnutall1@gmail.com>
2024-01-08 14:00:54 -0600

    working on css

--- cuttin corner/src/App.jsx
+++ cuttin corner/src/App.jsx
@@ -1,24 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import nutLogo from './assets/20180320_165555962_iOS.png'
-import './App.css'
+import { useState } from "react";
+import nutLogo from "./assets/20180320_165555962_iOS.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={nutLogo} className="logo" alt=" logo" />
-          </a>
-        
+        <img src={nutLogo} className="logo" alt=" logo" />
       </div>
 
-      
       <div className="card">
-      <p><h1>Cuttin Corner</h1></p>
-      
+        <p>
+          <h1>Cuttin Corner</h1>
+        </p>
+
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -29,10 +26,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <footer><h5>Developed by Rodney "Rodzilla" Nutall </h5></footer>
-      
+      <footer>
+        <h5>Developed by Rodney "Rodzilla" Nutall </h5>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
