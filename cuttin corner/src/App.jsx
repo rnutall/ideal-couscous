1f96ab4ec79a0da95726283eb07065d1f433c840
rnutall <rnutall1@gmail.com>
2024-01-08 13:25:54 -0600

    added added logo to header adjusted footer

--- cuttin corner/src/App.jsx
+++ cuttin corner/src/App.jsx
@@ -11,13 +11,13 @@ function App() {
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={nutLogo} className="logo" alt=" logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          </a>
+        
       </div>
-      <h1>Cuttin Corner</h1>
+
+      
       <div className="card">
+      <p><h1>Cuttin Corner</h1></p>
       
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -29,12 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <footer> <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt=" Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a></footer>
+      <footer><h5>Developed by Rodney "Rodzilla" Nutall </h5></footer>
       
     </>
   )
