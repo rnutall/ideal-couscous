ed609e83481a51f9a7787d4a41a340ed10c89335
rnutall <rnutall1@gmail.com>
2024-01-10 13:05:26 -0600

    added carousel of images had to firgue out how to place it the head would disappear when code was enterd to the <div>.

---
+++ cuttin corner/images.jsx
@@ -0,0 +1,21 @@
+// Filename - images.jsx
+
+// Replace src value with your image url
+const images = [
+	{
+		id: 1,
+		src: "./assets/WP_20151230_15_57_44_Pro.jpg",
+		alt: "Image 1",
+	},
+	{
+		id: 2,
+		src: "Images/2.png",
+		alt: "Image 2 ",
+	},
+	{
+		id: 3,
+		src: "Images/3.png",
+		alt: "Image 3",
+	},
+];
+export default images;
