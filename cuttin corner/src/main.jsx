add4d30092616479b2a7ba8144b94d206cf1fbfe
rnutall <rnutall1@gmail.com>
2024-07-18 18:38:20 -0500

    added systems-observable

--- cuttin corner/src/main.jsx
+++ cuttin corner/src/main.jsx
@@ -1,7 +1,9 @@
+import 'system-observable'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -9,3 +11,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>,
   );
 
+  document.getElementById('root')
